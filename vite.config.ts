6c335cdc1a255af22b8a5bc23316be364cac6e76
Shubham Maurya <shubhammr21@gmail.com>
2025-01-08 05:01:52 +0530

    feat(dependencies): add tanstack react-query, react-router and devtools
    - Add `@tanstack/react-query`, `@tanstack/react-query-devtools`, and `@tanstack/react-router` dependencies
    - Install development dependencies `@tanstack/router-devtools` and `@tanstack/router-plugin`
    - Update Vite configuration to include `TanStackRouterVite` plugin
    
    This change introduces new dependencies for state management, routing, and developer tooling from the Tanstack library. The `react-query` library provides efficient and robust state management for asynchronous data, while `react-router` enables declarative routing within the application. Additionally, the corresponding developer tools are included to aid in debugging and inspection during development.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import path from "node:path"
 import react from "@vitejs/plugin-react-swc"
+import path from "node:path"
 import { defineConfig } from "vite"
+
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), TanStackRouterVite()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
 })
