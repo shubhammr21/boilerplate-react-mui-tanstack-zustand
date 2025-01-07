ed26b9a8ccc232ed24a076729cc74011a28b7a35
Shubham Maurya <shubhammr21@gmail.com>
2025-01-08 03:11:02 +0530

    feat(ui): migrate to material ui with basic configuration
    Summary: This commit introduces Material UI as the UI library for the React application. It sets up the necessary dependencies and updates the application structure to incorporate Material UI components and theming.
    
    Key points:
    
    - Install required Material UI packages: @emotion/react, @emotion/styled, @mui/icons-material, @mui/material
    - Replace the existing App component with a new one using Material UI components (Button, Container, Typography, etc.)
    - Add a custom LightBulbIcon component using Material UI's SvgIcon
    - Create a ProTip and Copyright components for displaying additional information
    - Set up a custom theme using Material UI's createTheme function
    - Wrap the application with ThemeProvider and CssBaseline from Material UI
    
    The migration to Material UI aims to provide a modern, responsive, and accessible UI for the application. It leverages Material Design principles and offers a comprehensive set of UI components and utilities, enabling consistent styling and behavior across the application. The custom theme allows for further customization and branding of the UI elements.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
+import { CssBaseline } from "@mui/material"
+import { ThemeProvider } from "@mui/material/styles"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import App from "./App.tsx"
+import App from "./App"
 import "./index.css"
+import theme from "./theme"
 
 const rootElement = document.getElementById("root")
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </StrictMode>,
   )
 } else {
---
+++ src/theme/index.ts
@@ -0,0 +1,8 @@
+import { createTheme } from "@mui/material/styles"
+
+// A custom theme for this app
+const theme = createTheme({
+  cssVariables: true
+})
+
+export default theme
